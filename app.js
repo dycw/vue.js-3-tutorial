042375277b0ef82d4a71dc971de45157e7053e69
Derek Wan <d.wan@icloud.com>
2023-05-13 21:13:38 +0800

    Finish lesson 3 (Vue.js basics, part 2)

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.thenetninja.co.uk",
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      books: [
+        {
+          title: "Name of the Wind",
+          author: "Patrick Rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
