134c9dc83f3d310d3e145e86567a0fe0f58a8432
Derek Wan <d.wan@icloud.com>
2023-05-14 23:39:32 +0800

    Finish lesson 11 (the composition API, part 2) (#11)

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import DetailsView from "../views/DetailsView";
+import HomeView from "../views/HomeView";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
+  { path: "/", name: "Home", component: HomeView },
+  { path: "/posts/:id", name: "Details", component: DetailsView, props: true },
 ];
 
 const router = createRouter({
